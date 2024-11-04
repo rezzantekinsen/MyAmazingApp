b878d5cfbc2478a6ef98084e29d4412acfade29f
Rezzan Tekinsen <rezzantekinsen06@gmail.com>
2024-11-04 15:19:24 +0100

    Initial Commit

---
+++ MyAmazingApp/MyAmazingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAmazingAppApp.swift
+//  MyAmazingApp
+//
+//  Created by Rezzan Tekinsen on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MyAmazingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
